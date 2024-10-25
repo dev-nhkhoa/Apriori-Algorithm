fc04b0c691a424832055810cb06aab897e8561e1
nhkhoa <work.nhkhoa@gmail.com>
2024-10-25 15:47:11 +0700

    Navigation

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ if __name__ == '__main__':
     st.Page("./pages/introduce.py", title="Giới thiệu", default=True),
     st.Page("./pages/apriori_algorithm.py", title="Thuật toán Apriori"),
     st.Page("./pages/generate_sample_data.py", title="Tạo dữ liệu mẫu"),
-    st.Page("./pages/market_basket_dataset.py", title="Apriori trên các bộ dữ liệu"),
+    st.Page("./pages/apriori_on_datasets.py", title="Apriori trên các bộ dữ liệu"),
+    st.Page("./pages/market_basket_dataset.py", title="Bộ dữ liệu market Basket Dataset"),
+    st.Page("./pages/country_level_purchase_dataset.py", title="Country Level Purchase Dataset"),
+    st.Page("./pages/brazilian_ecommerce.py", title="Brazilian E-commerce Dataset"),
+    
 ])
 pg.run()
